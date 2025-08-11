4ae6c908b959e6cb07cd3513339dab311802e164
Lucas Martin Gutierrez <lucasgut3001@gmail.com>
2025-08-11 16:54:04 -0300

    Enviroments variables

--- Client/src/app/services/auth.service.ts
+++ Client/src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, Observable, throwError } from 'rxjs';
 import { jwtDecode } from 'jwt-decode';
-import { environment } from '../../enviroments/environment.prod';
+import { environment } from '../../enviroments/environment';
 
 @Injectable({
   providedIn: 'root'
